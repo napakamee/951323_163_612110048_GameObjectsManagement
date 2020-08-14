0ba86bd661c0ec9a85b8468393e5a8d9994a1242
DESKTOP-325D3AA\612110048 <napakamee_n@cmu.ac.th>
2020-08-14 16:42:51 +0700

    updated

---
+++ Assets/Scripts/ItemType.cs
@@ -0,0 +1,6 @@
+﻿public enum ItemType {
+    SPHERE_DROP_ITEM ,
+    CUBE_OBSTACLE ,
+    CAPSULE_OBSTACLE ,
+    CYLINDER_OBSTACLE ,
+}
---
+++ Assets/Scripts/ItemTypeComponent.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemTypeComponent : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    protected ItemType _itemType;
+    public ItemType Type{
+        get {
+            return _itemType;
+        }
+        set{
+            _itemType = value;
+        }
+    }
+}
